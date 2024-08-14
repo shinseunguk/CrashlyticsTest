dfe71d31aabc01861e13394d6ad93cee8756dfa1
신승욱 <ukseung.dev@incross.com>
2024-08-14 09:26:37 +0900

    Initial Commit

---
+++ CrashlyticsTest/CrashlyticsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrashlyticsTestApp.swift
+//  CrashlyticsTest
+//
+//  Created by ukseung.dev on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CrashlyticsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
